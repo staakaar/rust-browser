bde54a1f663fd41f15d76a2e09236cd1a1f7cdf6
staakaar <vancyroletaka@gmail.com>
2025-01-11 01:06:39 +0900

    add: http client impl

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,7 +1,45 @@
+extern crate alloc;
+use alloc::format;
+use alloc::string::String;
+use alloc::string::ToString;
+use noli::net::lookup_host;
+use noli::net::SocketAddr;
+use noli::net::TcpStream;
+use saba_core::error::Error;
+use saba_core::http::HttpClient;
+
 pub struct HttpClient {}
 
 impl HttpClient {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::new(format!("Failed to resolve host: {:#?}", e)))
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::NetWork("Failed to find IP addresses".to_string()));
+        }
+
+        let socket_addr: SocketAddr = (ips[0], port).into();
+
+        let mut stream = match TcpStream::connect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::NetWork(
+                    "Failed to connect to TCP stream:".to_string(),
+                ))
+            }
+        };
+
+        let mut request = String::from("GET /");
+        request.push_str(&path);
+        request.push_str(" HTTP/1.1\n");
     }
 }
