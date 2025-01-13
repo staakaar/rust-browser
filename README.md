5385cfe5fa1b40c4f6f8d633dd2ac1d3e38fc7ec
staakaar <vancyroletaka@gmail.com>
2025-01-14 00:25:21 +0900

    add: http client setup

---
+++ README.md
@@ -0,0 +1,7 @@
+#
+
+## ローカルサーバー実行
+
+```shell
+python3 -m http.server 8000
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #![no_std]
+#![no_main]
 #![cfg_attr(not(target_os = "linux"), no_main)]
 
+extern crate alloc;
+
+use crate::alloc::string::ToString;
+use net_wasabi::http::HttpClient;
 use noli::prelude::*;
 
-fn main() {
+fn main() -> u64 {
     Api::write_string("Hello World\n");
     println!("Hello, from println!");
     Api::exit(42);
+
+    let client = HttpClient::new();
+    match client.get("example.com".to_string(), 80, "/".to_string()) {
+        Ok(res) => {
+            print!("response:\n{:#?}", res);
+        }
+        Err(e) => {
+            print!("error:\n{:#?}", e);
+        }
+    }
+    ()
 }
 
 entry_point!(main);
