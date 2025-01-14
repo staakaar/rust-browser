3486881baf641565b5c54069b778ad13775cee7b
staakaar <vancyroletaka@gmail.com>
2025-01-15 00:52:06 +0900

    fix: host name

--- run_on_wasabi.sh
+++ run_on_wasabi.sh
@@ -3,36 +3,40 @@
 HOME_PATH=$PWD
 TARGET_PATH=$PWD"/build"
 OS_PATH=$TARGET_PATH"/wasabi"
-APP_NAME="rust-browser"
+APP_NAME="saba"
 MAKEFILE_PATH=$HOME_PATH"/Makefile"
 
-# buildディレクトリを作成する
-if [ -d $TARGET_PATH]
+# execute `mkdir build/` if it doesn't exist
+if [ -d $TARGET_PATH ]
 then
-    echo $TARGET_PATH" exists"
+  echo $TARGET_PATH" exists"
 else
-    echo $TARGET_PATH" doesn't exist"
-    mkdir $TARGET_PATH
+  echo $TARGET_PATH" doesn't exist"
+  mkdir $TARGET_PATH
 fi
 
+# install Wasabi OS (https://github.com/hikalium/wasabi)
+# You should manually remove wasabi/ if it's conflict via `rm -rf $OS_PATH`
 if [ -d $OS_PATH ]
 then
-    echo $OS_PATH" exists"
-    echo "pulling new changes..."
-    cd $OS_PATH
-    git pull origin for_saba
+  echo $OS_PATH" exists"
+  echo "pulling new changes..."
+  cd $OS_PATH
+  git pull origin for_saba
 else
-    echo $OS_PATH" doesn't exist"
-    echo "cloning wasabi project..."
-    cd $TARGET_PATH
-    git clone --branch for_saba git@github.com:hikalium/wasabi.git
+  echo $OS_PATH" doesn't exist"
+  echo "cloning wasabi project..."
+  cd $TARGET_PATH
+  git clone --branch for_saba git@github.com:hikalium/wasabi.git
 fi
 
+# go back to the application top directory
 cd $HOME_PATH
 
+# download Makefile if it doesn't exist
 if [ ! -f $MAKEFILE_PATH ]; then
-    echo "downloading Makefile..."
-    curl https://raw.githubusercontent.com/hikalium/wasabi/main/external_app_template/Makefile
+  echo "downloading Makefile..."
+  curl https://raw.githubusercontent.com/hikalium/wasabi/for_saba/external_app_template/Makefile
 fi
 
 make build
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() -> u64 {
     Api::exit(42);
 
     let client = HttpClient::new();
-    match client.get("example.com".to_string(), 80, "/".to_string()) {
+    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
         Ok(res) => {
             print!("response:\n{:#?}", res);
         }
